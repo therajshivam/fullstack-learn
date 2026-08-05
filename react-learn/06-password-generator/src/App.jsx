126bc0602942cbf3769a5706ae2f6ae643f3919b
Shivam Raj <rajshivam5984@gmail.com>
2026-08-05 13:28:24 +0530

    password-gen

--- react-learn/06-password-generator/src/App.jsx
+++ react-learn/06-password-generator/src/App.jsx
@@ -3,16 +3,83 @@ import './App.css'
 
 function App() {
   const [length, setLength] = useState(8)
-  const [numberAllowed, setnumberAllowed] = useState(false)
-  const [charAllowed, setcharAllowed] = useState(false)
-  const [password, setPassword] = useState("")
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("") // input field password
+
+  // password generator method
+  // we have to call this method many times(numbers, length, chars), for the optimised way : useCallback()
+  const passwordGenerator = useCallback(()=>{
+    let pass =""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*()_+-?=/[]{.}>|;':"
+
+    for(let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random * str.length + 1)
+      pass = str.charAt(char)
+    }
+
+    setPassword(pass)
+
+  }, [length, numberAllowed, charAllowed, setLength])
 
 
 
   return (
     <>
-    <h1 className='text-4xl text-center text-white'>
-    Password Generator</h1>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-white bg-gray-700'>
+      <h1 className='text-center text-white text-2xl m-2'>Password Generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input 
+        type="text"
+        value={password}
+        className='outline-none w-full py-1 px-3 bg-amber-50 text-gray-950'
+        placeholder='Password'
+        readOnly
+        />
+        <button
+        className='outline-none bg-blue-700 px-3 py-0.5 shrink-0 '
+        >Copy</button>
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input 
+          type="range"
+          min={6}
+          max={50}
+          value={length}
+          className='cursor-pointer'
+          onChange={(e)=>{setLength(e.target.value)}}
+          />
+          <label>Length: {length}</label>
+        </div>
+        <div 
+        className='flex items-center gap-x-1'>
+          <input 
+          type="checkbox" 
+          defaultChecked={numberAllowed} 
+          id='numberInput'
+          onChange={()=>{
+            setNumberAllowed((prev) => !prev)
+          }}
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div 
+        className='flex items-center gap-x-1'>
+          <input 
+          type="checkbox" 
+          defaultChecked={charAllowed} 
+          id='charInput'
+          onChange={()=>{
+            setCharAllowed((prev) => !prev)
+          }}
+          />
+          <label htmlFor="charInput">Characters</label>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
--- react-learn/notes/09-notes.md
+++ react-learn/notes/09-notes.md
@@ -1,11 +1,17 @@
+# useEffect, useRef and useCallback
 
+## Password Generator Project
 
+[Code](../06-password-generator/src/App.jsx)
+
+Objectives: 
 - methods run after the page load.
 - dependencies like number, characters and length slider, changes in any these dependencies will run the method again.
 - how to do optimised way.
 - how to target any feed (eg. copy the input).
 
-Problem : There is a password generator function. And we have to call it many times like when clicking numbers and chars. Find any way to optimised password generator method as it is calling many times.   
-Solution : useCallback(fn, dependencies)  
+*Problem* : There is a password generator function. And we have to call it many times like when clicking numbers and chars. Find any way to optimised password generator method as it is calling many times.   
+
+*Solution* : `useCallback(fn, dependencies)`  
 useCallback is a react hook that lets us cache a function defination between re-renders.
 
