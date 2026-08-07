da37eeb3e8cb28f74aa71dd66c5c104a1f5801f8
Shivam Raj <rajshivam5984@gmail.com>
2026-08-07 23:34:39 +0530

    password-generator

--- react-learn/06-password-generator/src/App.jsx
+++ react-learn/06-password-generator/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     let pass =""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     if (numberAllowed) str += "0123456789"
-    if (charAllowed) str += "!@#$%^&*()_+-?=/[]{.}>|;'`:"
+    if (charAllowed) str += "!@#$%^&*()_+-?=/[]{}.>|;'`:"
 
     for(let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
