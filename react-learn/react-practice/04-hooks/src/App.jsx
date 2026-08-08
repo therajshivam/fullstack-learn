c34bc76c172ce5790bb3c22114477bf516d36c41
Shivam Raj <rajshivam5984@gmail.com>
2026-08-08 08:55:45 +0530

    hooks practice

---
+++ react-learn/react-practice/04-hooks/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='min-h-screen w-full bg-gray-900 text-white '>
+      <h1 className='text-center text-4xl p-5'>Hooks</h1>
+    </div>
+  )
+}
+
+export default App
