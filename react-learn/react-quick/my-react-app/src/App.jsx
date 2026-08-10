8319168c15e7077e1546fd8e4dfae3a1996ea498
Shivam Raj <rajshivam5984@gmail.com>
2026-08-10 12:49:18 +0530

    update

--- react-learn/react-quick/my-react-app/src/App.jsx
+++ react-learn/react-quick/my-react-app/src/App.jsx
@@ -1,15 +1,12 @@
-import Header from "./Header"
-import Footer from "./Footer"
-import Food from "./Food"
+import Card from "./Card"
+
 
 function App() {
           
 
   return (
     <>
-      <Header />
-      <Food />
-      <Footer />
+      <Card />
     </>
   )
 }
---
+++ react-learn/react-quick/my-react-app/src/Card.jsx
@@ -0,0 +1,9 @@
+function Card() {
+    return(
+        <div className="card">
+            <img src="" alt="" />
+        </div>
+    )
+}
+
+export default Card
