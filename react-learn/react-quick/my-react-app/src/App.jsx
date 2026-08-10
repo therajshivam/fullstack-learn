3a453b648c51ecf6cbf925a496baf2c9c78c3b83
Shivam Raj <rajshivam5984@gmail.com>
2026-08-10 21:48:13 +0530

    lists'

--- react-learn/react-quick/my-react-app/src/App.jsx
+++ react-learn/react-quick/my-react-app/src/App.jsx
@@ -1,22 +1,28 @@
 // import Student from "./Student"
-import UserGreeting from "./UserGreeting"
+// import UserGreeting from "./UserGreeting"
+import List from "./List"
 
 
 function App() {
           
 
-  return (
-    // <>
-    //   <Student name="Spongebob" age={30} isStudent={true} />
-    //   <Student name="Patrick" age={42} isStudent={false} />
-    //   <Student name="Squidward" age={50} isStudent={false} />
-    //   <Student name="Sandy" age={27} isStudent={true} />
-    //   <Student  />
-    // </>
+  // return (
+  //   // <>
+  //   //   <Student name="Spongebob" age={30} isStudent={true} />
+  //   //   <Student name="Patrick" age={42} isStudent={false} />
+  //   //   <Student name="Squidward" age={50} isStudent={false} />
+  //   //   <Student name="Sandy" age={27} isStudent={true} />
+  //   //   <Student  />
+  //   // </>
 
-    <>
-      <UserGreeting isLoggedIn={false} username="BroCode" />
-    </>
+  //   // <>
+  //   //   <UserGreeting isLoggedIn={false} username="BroCode" />
+  //   // </>
+
+  // )
+
+  return (
+    < List />
   )
 }
 
---
+++ react-learn/react-quick/my-react-app/src/List.jsx
@@ -0,0 +1,23 @@
+// render list
+
+function List () {
+    // const fruits = ['apple', 'banana', 'papaya', 'orange', 'mango']
+    // const listItems = fruits.map(fruits => <li>{fruits}</li>)
+    // return(<ol>{listItems}</ol>)
+
+    const fruits = [{id : '1',name :'apple', calories : 95}, 
+                    {id : '2',name : 'banana', calories : 45}, 
+                    {id : '3',name : 'papaya', calories : 105}, 
+                    {id : '4',name : 'orange', calories : 159}, 
+                    {id : '5',name : 'mango', calories : 37}]
+
+    // fruits.sort((a,b)=> a.name.localeCompare(b.name)) // Alphabatical
+    fruits.sort((a,b)=> a.name.localeCompare(a.name)) // reverse Alphabatical
+    const listItems = fruits.map(fruits => <li key= {fruits.id}>
+                                            {fruits.name}: &nbsp; 
+                                            <b>{fruits.calories}</b></li>)
+    return(<ol>{listItems}</ol>)
+
+} 
+
+export default List
